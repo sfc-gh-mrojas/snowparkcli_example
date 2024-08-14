3b954c4a899ec5ca09b690efc39516440cd058ec
orellabac <mauricio.rojas@snowflake.com>
2024-08-13 21:19:33 -0600

    fixes for loading file from zip

--- python_package/snowpark_project/src/common.py
+++ python_package/snowpark_project/src/common.py
@@ -4,23 +4,19 @@ import yaml
 def project_variables(filename: str):
     import snowpark_project
     #files = os.listdir(os.path.dirname(snowpark_project.__file__))
-    return os.path.join(os.path.dirname(snowpark_project.__file__), filename)
-    #yaml_data = load_data_from_package(snowpark_project, filename)
-    #yaml_content = yaml.safe_load(yaml_data)
-    #return f"{files}"
+    #return os.path.join(os.path.dirname(snowpark_project.__file__), filename)
+    yaml_data = load_data_from_package(snowpark_project, filename)
+    yaml_content = yaml.safe_load(yaml_data)
+    return yaml_content
 
 
 def load_data_from_package(package, filename):
     try:
         # Attempt to construct the file path directly
         data_path = os.path.join(os.path.dirname(package.__file__), filename)
-        if os.path.exists(data_path):
-            with open(data_path, 'rt') as file:
-                return file.read()
-        else:
-            # Path does not exist, likely a zip import
-            raise FileNotFoundError
-    except (AttributeError, FileNotFoundError):
+        with open(data_path, 'rt') as file:
+            return file.read()
+    except:
         # __file__ is not set or path doesn't exist, use the loader
         if hasattr(package, '__loader__'):
             return package.__loader__.get_data(filename).decode('utf-8')
--- python_package/snowpark_project/src/procedures.py
+++ python_package/snowpark_project/src/procedures.py
@@ -1,7 +1,5 @@
 from snowflake.snowpark import Session
 
 from snowpark_project.src.common import project_variables
-
-
 def hello_procedure(session: Session) -> str:
-    return f"{project_variables('project.yml')}"
+    return f"{project_variables('snowpark_project/project.yml')}"
