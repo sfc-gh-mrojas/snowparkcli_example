f0dd820e44dfdc8e11f764666e05067adbaea44e
Miles Adkins <miles.adkins@snowflake.com>
2024-08-13 21:52:25 -0500

    first commit

---
+++ python_package/snowpark_project/src/procedures.py
@@ -0,0 +1,7 @@
+from snowflake.snowpark import Session
+
+from snowpark_project.src.common import project_variables
+
+
+def hello_procedure(session: Session) -> str:
+    return f"{project_variables('project.yml')}"
